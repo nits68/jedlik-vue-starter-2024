911c8d81261ecf96a3d4069f689cbf734a95cac3
nits68 <nits.laszlo@jedlik.eu>
2024-11-24 09:44:17 +0100

    add more example

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -15,6 +15,10 @@ const configCircle: Ref<CircleConfig> = ref({
   fill: 'red',
   stroke: 'black',
   strokeWidth: 4,
+  shadowColor: 'gray',
+  shadowOffset: { x: 10, y: 10 },
+  shadowBlur: 10,
+  shadowOpacity: 0.5,
 });
 </script>
 
@@ -26,6 +30,38 @@ const configCircle: Ref<CircleConfig> = ref({
       <v-stage :config="configKonva">
         <v-layer>
           <v-circle :config="configCircle"></v-circle>
+          <v-text :config="{ text: 'Some text on canvas', fontSize: 15 }" />
+          <v-rect
+            :config="{
+              x: 20,
+              y: 50,
+              width: 100,
+              height: 100,
+              fill: 'blue',
+              shadowBlur: 10,
+            }"
+          />
+          <v-circle
+            :config="{
+              x: 200,
+              y: 100,
+              radius: 50,
+              fill: 'green',
+            }"
+          />
+          <v-line
+            :config="{
+              x: 20,
+              y: 200,
+              points: [0, 0, 100, 0, 100, 100],
+              tension: 0.5,
+              closed: true,
+              stroke: 'black',
+              fillLinearGradientStartPoint: { x: -50, y: -50 },
+              fillLinearGradientEndPoint: { x: 50, y: 50 },
+              fillLinearGradientColorStops: [0, 'red', 1, 'yellow'],
+            }"
+          />
         </v-layer>
       </v-stage>
     </div>
